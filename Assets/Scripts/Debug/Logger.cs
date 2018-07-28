b60add5eec309c493c209019bb633c7b2ed73914
Maxim <maxkarab@yandex.ru>
2018-07-28 08:55:18 +0400

    Добавлен класс для логирования на экран и в файл лога. Добавлена прототипная система спауна препятствий.

---
+++ Assets/Scripts/Debug/Logger.cs
@@ -0,0 +1,77 @@
+﻿namespace Game.Debug
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using System.Text;
+    using System.IO;
+    using System;
+    public enum LogTypes
+    {
+        Info = 1,
+        Warning = 2,
+        Error = 3
+    }
+
+    /// <summary>
+    /// Класс-обертка класса Debug для упрощения логирования при отладке.
+    /// </summary>
+    public static class Logger
+    {
+        private static string logFileName = "Debug/Debug.log";
+        private static void Log(LogTypes type, GameObject sender, string log)
+        {
+            log = string.Format("{0} Объект: {1}", log, sender.name);
+            Display(type, log);
+            WriteToFile(type, log);
+        }
+        private static void Log(LogTypes type, GameObject sender, string format, params object[] args)
+        {
+            string log = string.Format(format, args);
+            log = string.Format("{0} Объект: {1}", log, sender.name);
+            Display(type, log);
+            WriteToFile(type, log);
+        }
+        public static void LogInfo(GameObject sender, string log)
+        {
+            Log(LogTypes.Info, sender, log);
+        }
+        public static void LogWarning(GameObject sender, string log)
+        {
+            Log(LogTypes.Warning, sender, log);
+        }
+        public static void LogError(GameObject sender, string log)
+        {
+            Log(LogTypes.Error, sender, log);
+        }
+
+        private static void Display(LogTypes type, string log)
+        {
+            switch (type)
+            {
+                case LogTypes.Info:
+                    Debug.Log(log);
+                    break;
+                case LogTypes.Warning:
+                    Debug.LogWarning(log);
+                    break;
+                case LogTypes.Error:
+                    Debug.LogError(log);
+                    break;
+                default:
+                    break;
+            }
+        }
+        private static void WriteToFile(LogTypes type, string log)
+        {
+            log = string.Format("{0}: {1}! {2}\n", DateTime.Now, type.ToString(), log);
+            using (FileStream stream = new FileStream(logFileName, FileMode.Append, FileAccess.Write))
+            {
+                byte[] bytes = Encoding.Default.GetBytes(log);
+                stream.Write(bytes, 0, bytes.Length);
+                stream.Flush();
+            }
+        }
+    }
+}
+
--- Assets/Scripts/Game/BGSpawnBorder.cs
+++ Assets/Scripts/Game/BGSpawnBorder.cs
@@ -1,44 +1,94 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class BGSpawnBorder : MonoBehaviour
+﻿namespace Game
 {
-    [SerializeField] private GameObject backgroundPrefab;
-    [SerializeField] private Transform spawnPoint;
-    private GameObject prev;
-
-    private void Awake()
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using System.Linq;
+    using Debug;
+    using System;
+    public class BGSpawnBorder : MonoBehaviour
     {
-        if (backgroundPrefab == null)
-        {
-            Debug.LogError(string.Format("Backgound prefab не инициализирован. Объект {0}", name));
-        }
-    }
+        [SerializeField] private GameObject backgroundPrefab;
+        [SerializeField] private Transform spawnPoint;
+        private GameObject[] topObstaclePoints;
+        private GameObject[] bottomObstaclePoints;
+        [SerializeField] private string topObstaclePointsTag;
+        [SerializeField] private string bottomObstaclePointsTag;
+        [SerializeField] private GameObject[] obstaclePrefabs;
+        [SerializeField] private int topObstaclesCount;
+        [SerializeField] private int bottomObstaclesCount;
+        [SerializeField] private Vector3 obstacleScale = new Vector3(.1f, .1f, .1f);
+        private GameObject prev;
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.tag == "Player")
+        private void Awake()
         {
-            if(this.Prev != null)
+            topObstaclePoints = GameObject.FindGameObjectsWithTag(topObstaclePointsTag);
+            bottomObstaclePoints =  GameObject.FindGameObjectsWithTag(bottomObstaclePointsTag);
+            if (topObstaclePoints == null || bottomObstaclePoints == null || !topObstaclePoints.Any() || !bottomObstaclePoints.Any())
             {
-                Destroy(this.prev);
+                Debug.Logger.LogError(this.gameObject, "Точки появления препятсткий не инициализированы.");
             }
-            
-            GameObject bg = Instantiate(backgroundPrefab, spawnPoint.position, Quaternion.identity);
-            bg.GetComponentInChildren<BGSpawnBorder>().Prev = this.gameObject;
-        }
-    }
 
-    public GameObject Prev
-    {
-        get
-        {
-            return prev;
+            SpawnObstacles(topObstaclePoints, obstaclePrefabs, topObstaclesCount);
+            SpawnObstacles(bottomObstaclePoints, obstaclePrefabs, bottomObstaclesCount);
+
+            if (backgroundPrefab == null)
+            {
+                Debug.Logger.LogError(this.gameObject, "BackgroundPrefab не инициализирован.");
+            }
         }
-        set
+
+        private void SpawnObstacles(GameObject[] points, GameObject[] prefabs, int maxCount)
         {
-            prev = value;
+            System.Random rand = new System.Random();
+            int obsCont = 0;
+            for (int i = 0; i < points.Length; i++)
+            {
+                int val = rand.Next(0, 100);
+                if (val > 30 && obsCont < maxCount)
+                {
+                    try
+                    {
+                        val = rand.Next(0, obstaclePrefabs.Length);
+                        GameObject obs = Instantiate(prefabs[val], points[i].transform.position, Quaternion.identity, this.transform.parent);
+                        obs.transform.localScale = this.obstacleScale;
+                        obsCont++;
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.Logger.LogError(this.gameObject, ex.Message);
+                        break;
+                    }
+
+                }
+            }
+        }
+
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.tag == "Player")
+            {
+                if (this.Prev != null)
+                {
+                    Destroy(this.prev);
+                }
+
+                GameObject bg = Instantiate(this.backgroundPrefab, this.spawnPoint.position, Quaternion.identity);
+                bg.GetComponentInChildren<BGSpawnBorder>().Prev = this.gameObject;
+            }
+        }
+
+        public GameObject Prev
+        {
+            get
+            {
+                return prev;
+            }
+            set
+            {
+                prev = value;
+            }
         }
     }
 }
